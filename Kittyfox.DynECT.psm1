569423186d4a6520fa79dc76853e2c02db66066c
Wroten <D01057054@dvuadmin.net>
2017-11-13 17:17:10 -0600

    - Added Region Blocks - Added Commandlets for Zone Changes, A Records (completed some methods) - Modified logic of Session commandlets to better handle reconnect scenario - Initial addition of Rate Limit helper methods

--- Kittyfox.DynECT.psm1
+++ Kittyfox.DynECT.psm1
@@ -11,11 +11,22 @@ $DynECTPassword = [SecureString]::new()
 ## Configurable Options
 $DynECTSessionCheckInterval = 60
 
+$DynECTRateLimitPerSecond = 5
+$DynECTRateLimitPerMinute = 300
+
+$DynECTRatePerSecond = 0
+$DynECTRatePerMinute = 0
+
+$DynECTSecondLimitStart = (Get-Date)
+$DynECTMinuteLimitStart = (Get-Date)
+
 <# Known Types of the Module
 
  Kittyfox.DynECT.ZoneInfo
+ Kittyfox.DynECT.ZoneChangeInfo
+ Kittyfox.DynECT.ZonePublishInfo
 
-
+ Kittyfox.DynECT.ARecordInfo
 
 #>
 
@@ -25,8 +36,33 @@ $DynECTSessionCheckInterval = 60
         $Item.PSObject.TypeNames.Insert(0,'Kittyfox.DynECT.Class') 
     }
 
+   New Commandlet Snippet
+
+Function FunctionName {
+    [CmdletBinding()]
+    Param(
+    )
+
+    throw [System.NotImplementedException]::new("This commandlet is not yet implemented.")
+}
+
 #>
 
+# Learn something new every day...
+#    https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.psmoduleinfo.onremove?view=powershellsdk-1.1.0
+#
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+
+    # We really need to disconnect prior to losing our token. Check, and disconnect if necessary. 
+    If (Test-DynECTSession) {
+        Write-Verbose 'Connected session detected. Disconnecting on unload.'
+
+        Disconnect-DynECTSession
+    }
+
+ }
+
+#region DynECT Session Commandlets
 Function Connect-DynECTSession {
     [CmdletBinding()]
     Param(
@@ -66,11 +102,22 @@ Function Connect-DynECTSession {
             $Script:DynECTPassword = $Password
         }
 
+        # Output the message from the remote side, verbose form.
+        $Response.msgs | foreach { Write-Verbose $_.INFO }
+
         Write-Verbose "Successful Login, Token: $AuthToken"
     } else {
         $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
 
         Write-Error $Message
+
+        # If we failed our connect AND we were persisting (whatever reason, blocked, user deleted, etc), wipe our persisted data
+        if ($Script:DynECTPersist) {
+            $Script:DynECTPersist = $False
+            $Script:DynECTCustomerName = [string]::Empty
+            $Script:DynECTUserName = [string]::Empty
+            $Script:DynECTPassword = [SecureString]::new()
+        }
     }
 }
 
@@ -83,42 +130,47 @@ Function Test-DynECTSession {
 
     $AuthToken = $Script:AuthToken
     $LastSessionCheck = $Script:LastSessionCheck
+    $CheckInterval = $Script:DynECTSessionCheckInterval
     $IsConnected = $False
 
     if (-not [string]::IsNullOrEmpty($AuthToken)) {
         Write-Verbose "Test Session, Token: $AuthToken"
 
-        if ((New-Timespan -Start $LastSessionCheck -End (Get-Date)).TotalSeconds -le $Script:DynECTSessionCheckInterval) {
+        # Buffer our calls to check. We don't need to assert it more than once a minute, maybe longer.
+        # Especially since many methods will make an assertion check using this commandlet.
+        if ((New-Timespan -Start $LastSessionCheck -End (Get-Date)).TotalSeconds -le $CheckInterval) {
             $IsConnected = $True
-            Write-Verbose "Skipped check, within the session check interval of $($Script:DynECTSessionCheckInterval)s since last test."
+            Write-Verbose "Skipped check, within the session check interval of $($CheckInterval)s since last test."
         } else {
             $Response = Helper-InvokeRestMethod -Method 'Get' -Uri '/Session'
 
             if ($Response.status -eq 'success') {
+                $Script:LastSessionCheck = (Get-Date)
                 $IsConnected = $True
                 Write-Verbose "Successfully Tested Session."
             } else {
-                if ($Reconnect -and $Script:DynECTPersist) {
-                    try {
-                        $ConnectParams = @{
-                            CustomerName = $Script:DynECTCustomerName
-                            UserName = $Script:DynECTUserName
-                            Password = $Script:DynECTPassword
-                        }
-                        Connect-DynECTSession @ConnectParams -ErrorAction Stop
-                    } catch {
-                        Write-Error "Reconnect requested, attempt failed: $($_.Exception.Message)"
-
-                        $Script:DynECTPersist = $False
-                        $Script:DynECTCustomerName = [string]::Empty
-                        $Script:DynECTUserName = [string]::Empty
-                        $Script:DynECTPassword = [SecureString]::new()
-                    }
-                }
+                # Our auth-token is expired. Remove it.
+                $Script:AuthToken = [string]::Empty
             }
         }
     } else {
         Write-Verbose "No Session Currently Detected."
+    }
+
+    if (-not $IsConnected -and $Reconnect -and $Script:DynECTPersist) {
+        try {
+            $ConnectParams = @{
+                CustomerName = $Script:DynECTCustomerName
+                UserName = $Script:DynECTUserName
+                Password = $Script:DynECTPassword
+            }
+            Connect-DynECTSession @ConnectParams -ErrorAction Stop
+            $IsConnected = $True
+        } catch {
+            Write-Error "Reconnect requested, attempt failed: $($_.Exception.Message)"
+
+            # What if we have persisted credentials and don't want to reconnect. Clear them or leave them?
+        }
     }
 
     Write-Output $IsConnected
@@ -140,13 +192,6 @@ Function Disconnect-DynECTSession {
             $Script:AuthToken = [string]::Empty
             $Script:SigninTime = $null
 
-            if ($Persistent) {
-                $Script:DynECTPersist = $False
-                $Script:DynECTCustomerName = [string]::Empty
-                $Script:DynECTUserName = [string]::Empty
-                $Script:DynECTPassword = [SecureString]::new()
-            }
-
             Write-Verbose "Succesfully Disconnected from DynECT and emptied state variables."
         } else {
             $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
@@ -154,8 +199,20 @@ Function Disconnect-DynECTSession {
             Write-Error $Message
         }
     }
-}
 
+    # We did this after a successful disconnect. I think it bears doing outside, as we can use Disconnect- as a way to clear
+    # persisted credentials, in case we timed out. Otherwise, perhaps unknowingly, a Test- using -Reconnect might reconnect our
+    # session long after we wanted it to be.
+    if ($Persistent) {
+        $Script:DynECTPersist = $False
+        $Script:DynECTCustomerName = [string]::Empty
+        $Script:DynECTUserName = [string]::Empty
+        $Script:DynECTPassword = [SecureString]::new()
+    }
+}
+#endregion DynECT Session Commandlets
+
+#region DynECT Zone Commandlets
 Function New-DynECTZone {
     [CmdletBinding()]
     Param(
@@ -208,7 +265,7 @@ Function Get-DynECTZone {
 
             if ($Response.status -eq 'success') {
                 if ($IsZoneSpecified) {
-                    $ZoneInfo = @{
+                    $ZoneInfo = [ordered] @{
                         Name = $Response.data.zone
                         Type = $Response.data.zone_type
                         SerialStyle = $Response.data.serial_style
@@ -291,6 +348,292 @@ Function Remove-DynECTZone {
     throw [System.NotImplementedException]::new("This commandlet is not yet implemented.")
 }
 
+Function Get-DynECTZoneChanges {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [string[]]
+        $Zone
+    )
+
+    Begin {
+        $UriBase = '/ZoneChanges'
+        $IsConnected = Test-DynECTSession -Reconnect
+
+        if (-not $IsConnected) {
+            Write-Error "Not connected to DynECT Managed DNS Service."
+            return
+        }
+    }
+
+    Process {
+        foreach ($Item in $Zone) {
+            $Uri = "$UriBase/$Item"
+
+            $Response = Helper-InvokeRestMethod -Method GET -Uri $Uri
+
+            if ($Response.status -eq 'success') {
+                # Output the message from the remote side, verbose form.
+                $Response.msgs | foreach { Write-Verbose $_.INFO }
+
+                foreach ($Change in $Response.data) {
+                    $ZoneChanges = [ordered] @{
+                        ID = $Change.id
+                        User = $Change.user_id
+                        Zone = $Change.zone
+                        FQDN = $Change.fqdn
+                        SourceSerial = $Change.serial
+                        TimeToLive = $Change.ttl
+                        RecordType = $Change.rdata_type
+                    }
+
+                    # Type appears to be the standard DNS Type abbreviation
+                    switch ($Change.rdata_type) {
+                        { $_ -in @('A', 'AAAA') } {
+                            $ZoneChanges.Add("Value", $Change.rdata."rdata_$($Change.rdata_type)".address)
+                        }
+                        "CNAME" {
+                            $ZoneChanges.Add("Value", $Change.rdata.rdata_cname.cname)
+                        }
+                        { $_ -in @("TXT", "SPF") } {
+                            $ZoneChanges.Add("Value", $Change.rdata."rdata_$($Change.rdata_type)".txtdata)
+                        }
+                        default {
+                            Write-Error "Change type '$($Change.rdata_type)' handler not yet implemented."
+                        }
+                    }
+                    $ZoneChangeData = New-Object -TypeName PSObject -Property $ZoneChanges
+                    $ZoneChangeData.PSObject.TypeNames.Insert(0,'Kittyfox.DynECT.ZoneChangeInfo')
+
+                    Write-Output $ZoneChangeData
+                }
+            } else {
+                $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
+
+                Write-Error $Message
+            }
+        }
+    }
+
+    End {
+    }
+}
+
+Function Clear-DynECTZoneChanges {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [string[]]
+        $Zone
+    )
+
+    Begin {
+        $UriBase = '/ZoneChanges'
+        $IsConnected = Test-DynECTSession -Reconnect
+
+        if (-not $IsConnected) {
+            Write-Error "Not connected to DynECT Managed DNS Service."
+            return
+        }
+    }
+
+    Process {
+        foreach ($Item in $Zone) {
+            $Uri = "$UriBase/$Item"
+
+            $Response = Helper-InvokeRestMethod -Method DELETE -Uri $Uri
+
+            if ($Response.status -eq 'success') {
+
+                # Output the message from the remote side, verbose form.
+                $Response.msgs | foreach { Write-Verbose $_.INFO }
+            } else {
+                $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
+
+                Write-Error $Message
+            }  
+        }
+    }
+
+    End {
+    }
+}
+#endregion DynECT Session Commandlets
+
+#region DynECT Record Commandlets
+Function New-DynECTARecord {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, Position=0)]
+        [string]
+        $Zone,
+
+        [Parameter(Mandatory=$true, Position=1, ValueFromPipelineByPropertyName=$True)]
+        [string[]]
+        $FQDN,
+
+        [Parameter(Mandatory=$true, Position=2, ValueFromPipelineByPropertyName=$True)]
+        [ValidateScript({[ipaddress]::TryParse($_, [ref] $null)})]
+        [string[]]
+        $Address,
+        
+        [Parameter(Mandatory=$true, Position=3, ValueFromPipelineByPropertyName=$True)]
+        [ValidateScript({[int]::TryParse($_, [ref] $null)})]
+        [string[]]
+        $TTL
+    )
+
+    Begin {
+        if ($FQDN.Count -ne $Address.Count -or $FQDN.Count -ne $TTL.Count) {
+            throw [System.ArgumentException]::New('FQDN, Address, and TTL must contain the same number of elements.')
+        }
+
+        $IsConnected = Test-DynECTSession -Reconnect
+
+        if (-not $IsConnected) {
+            Write-Error "Not connected to DynECT Managed DNS Service."
+            return
+        }
+    }
+
+    Process {
+        foreach ($Index in 0..($FQDN.Count-1)) {
+            $Name = $FQDN[$Index]
+            $Addr = $Address[$Index]
+            $Time = $TTL[$Index]
+
+            $Uri = "/ARecord/$Zone/$Name"
+            $RecordInfo = @{
+                rdata = @{address = $Addr}
+                ttl = $Time
+            }
+
+            $Response = Helper-InvokeRestMethod -Method POST -Uri $Uri -Body $RecordInfo
+
+            if ($Response.status -eq 'success') {
+                # Output the message from the remote side, verbose form.
+                $Response.msgs | foreach { Write-Verbose $_.INFO }
+
+                $RecordInfo = [ordered] @{
+                    Zone = $Response.data.zone
+                    FQDN = $Response.data.fqdn
+                    RecordType = $Response.data.record_type
+                    Address = $Response.data.rdata.address
+                    TTL = $Response.data.ttl
+                }
+                $RecordData = New-Object -Type PSObject -Property $RecordInfo
+                $RecordData.PSObject.TypeNames.Insert(0,'Kittyfox.DynECT.ARecordInfo')
+                Write-Output $RecordData
+            } else {
+                $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
+
+                Write-Error $Message
+            }
+        }
+    }
+
+    End {
+    }
+}
+
+Function Get-DynECTARecord {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, Position=0)]
+        [string]
+        $Zone,
+
+        [Parameter(Mandatory=$true, Position=1, ValueFromPipelineByPropertyName=$True)]
+        [string[]]
+        $FQDN,
+
+        [Parameter(Mandatory=$False, Position=2, ValueFromPipelineByPropertyName=$True)]
+        [string[]]
+        $RecordId
+    )
+
+    Begin {
+        $IsConnected = Test-DynECTSession -Reconnect
+
+        if (-not $IsConnected) {
+            Write-Error "Not connected to DynECT Managed DNS Service."
+            return
+        }
+    }
+
+    Process {
+        foreach ($Index in 0..($FQDN.Count-1)) {
+            $Name = $FQDN[$Index]
+            
+            if ($PSBoundParameters.ContainsKey('RecordId')) {
+                $Id = $RecordId[$Index]
+            }
+
+            $Uri = "/ARecord/$Zone/$Name"
+            if (-not [string]::IsNullOrEmpty($Id)) {
+                $ByRecordId = $True
+                $Uri = "$Uri/$Id"
+            }
+
+            $Response = Helper-InvokeRestMethod -Method GET -Uri $Uri
+
+            if ($Response.status -eq 'success') {
+                # Output the message from the remote side, verbose form.
+                $Response.msgs | foreach { Write-Verbose $_.INFO }
+
+                if ($ByRecordId) {
+                    $RecordInfo = [ordered] @{
+                        Zone = $Response.data.zone
+                        FQDN = $Response.data.fqdn
+                        RecordType = $Response.data.record_type
+                        Address = $Response.data.rdata.address
+                        TTL = $Response.data.ttl
+                    }
+                    $RecordData = New-Object -Type PSObject -Property $RecordInfo
+                    $RecordData.PSObject.TypeNames.Insert(0,'Kittyfox.DynECT.ARecordInfo')
+                    Write-Output $RecordData
+                } else {
+                    $NodeList = @()
+
+                    foreach ($Entry in $Response.data) {
+                        $FoundId = ($Entry.Split('/') | where { -not [string]::IsNullOrEmpty($_) })[-1]
+                        $NodeList += New-Object -Type PSObject -Property @{FQDN = $Name; RecordId = $FoundId}
+                    }
+
+                    $NodeList | Get-DynECTARecord -Zone $Zone
+                }
+            } else {
+                $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
+
+                Write-Error $Message
+            }
+
+        }
+    }
+
+    End {
+    }
+}
+
+Function Update-DynECTARecord {
+    [CmdletBinding()]
+    Param(
+    )
+
+    throw [System.NotImplementedException]::new("This commandlet is not yet implemented.")
+}
+
+Function Remove-DynECTARecord {
+    [CmdletBinding()]
+    Param(
+    )
+
+    throw [System.NotImplementedException]::new("This commandlet is not yet implemented.")
+}
+
+#endregion DynECT Record Commandlets
+
+#region DynECT Helper Commandlets
 Function Helper-UpdateDynECTZone {
     [CmdletBinding()]
     Param(
@@ -318,6 +661,7 @@ Function Helper-UpdateDynECTZone {
         $Notes
     )
 
+    $Uri = "/Zone/$Zone"
     $ZoneOptions = @{}
 
     switch ($PSCmdlet.ParameterSetName) {
@@ -337,11 +681,23 @@ Function Helper-UpdateDynECTZone {
     }
     $ZoneData = New-Object -TypeName PSObject -Property $ZoneOptions
 
-    # $Response = Helper-InvokeRestMethod -Method PUT -Uri $Uri -Body $ZoneData
+    $Response = Helper-InvokeRestMethod -Method PUT -Uri $Uri -Body $ZoneData
 
     if ($Response.status -eq 'success') {
         if ($PSCmdlet.ParameterSetName -eq 'Publish') {
-            
+            # Output the message from the remote side, verbose form.
+            $Response.msgs | foreach { Write-Verbose $_.INFO }
+
+            $ZoneInfo = [ordered] @{
+                Name = $Response.data.zone
+                Type = $Response.data.zone_type
+                PublishTask = $Response.data.task_id
+                SerialStyle = $Response.data.serial_style
+                SerialNumber = $Response.data.serial
+            }
+            $ZoneData = New-Object -Type PSObject -Property $ZoneInfo
+            $ZoneData.PSObject.TypeNames.Insert(0,'Kittyfox.DynECT.ZonePublishInfo')
+            Write-Output $ZoneData
         }
     } else {
         $Message = ($Response.msgs | where { $_.LVL -eq 'ERROR' }).INFO
@@ -393,7 +749,7 @@ Function Helper-InvokeRestMethod {
         ContentType = 'application/json'
     }
 
-    if ($Method -eq 'POST' -and $Body -ne $null) {
+    if ($Method -in @('POST', 'PUT') -and $Body -ne $null) {
         $JsonBody = ConvertTo-Json $Body
         $RestMethodParams.Add('Body', $JsonBody)
 
@@ -407,6 +763,10 @@ Function Helper-InvokeRestMethod {
     try {
         $Response = Invoke-RestMethod @RestMethodParams
     } catch {
+        # If we get a 429 Response code, we're ratelocked. Avoid sending requests during this time.
+        # Also, throw an error back to the calling code.
+        #   https://help.dyn.com/managed-dns-api-rate-limit/
+
         # Discovered this gem of a response from 
         #   https://stackoverflow.com/questions/18771424/how-to-get-powershell-invoke-restmethod-to-return-body-of-http-500-code-response
 
@@ -421,4 +781,13 @@ Function Helper-InvokeRestMethod {
     Write-Output $Response
 }
 
-Export-ModuleMember -Function Connect-*, Disconnect-*, Test-*, Get-*, Set-*, New-*, Remove-*
+Function Helper-DynECTRateLock {
+    [CmdletBinding()]
+    Param(
+    )
+
+    
+}
+#endregion DynECT Helper Commandlets
+
+Export-ModuleMember -Function Connect-*, Disconnect-*, Test-*, Get-*, Set-*, New-*, Remove-*, Lock-*, Unlock-*, Publish-*, Clear-*
